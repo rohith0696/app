c82bff301114ebdc0e8dc53b5066e2294395d8b3
Rohith0696 <rohithusa96@gmail.com>
2020-03-02 10:49:22 -0600

    challenge done

--- main.js
+++ main.js
@@ -10,3 +10,14 @@ function fun()
 {
     result.textContent = 'Your Name is '+name1.value+' '+name2.value;
 }
+const num1 = document.querySelector('#num1');
+
+const result1 = document.querySelector('#result1');
+
+btn1.addEventListener("click",calculate);
+
+function calculate()
+{
+    const cal =Math.PI*(num1.value*num1.value);
+    result1.textContent = 'Area is '+Math.round(cal*100.0)*100.0;
+}
